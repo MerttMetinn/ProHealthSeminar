f1a49b0ef5afd576ef4c10f6ffd9115f0720b5c3
kubra kilicaslan <133103452+kubrakilicaslan@users.noreply.github.com>
2024-02-25 23:10:18 +0300

    Delirdim galiba

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import Header from "./components/Header"
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from "./components/Login";
+import Header from "./components/Header";
 
 function App() {
-  
   return (
-    <div>
-      <Header/>
-      <div className="bg-indigo-600 w-full h-screen"></div>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/header" component={Header} />
+        <Route path="/" component={Login} />
+      </Switch>
+    </Router>
   )
 }
 
-export default App
+export default App;
